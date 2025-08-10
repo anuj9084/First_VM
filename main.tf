a43a9be90fb9ec1b45d532110dd1b22a88d03b80
Anuj Som <128492211+anuj9084@users.noreply.github.com>
2025-08-10 18:55:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
-resource "azurerm_resource_group" "example" {
-  name     = "rg-${var.env_name}"
+  resource "azurerm_resource_group" "demo_rg" {
+  name     = "anuj-rg"
   location = "East US"
+}
+
+resource "azurerm_virtual_network" "demo_vnet" {
+  name                = "anuj_vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.demo_rg.location
+  resource_group_name = azurerm_resource_group.demo_rg.name
+}
+
+resource "azurerm_subnet" "demo_subnet" {
+  name                 = "anuj_subnet"
+  resource_group_name  = azurerm_resource_group.demo_rg.name
+  virtual_network_name = azurerm_virtual_network.demo_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
